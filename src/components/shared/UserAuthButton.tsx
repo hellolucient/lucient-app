f9d2d7c48b3f26bcc9d137630d3deec0f4e7d31c
hellolucient <hellolucient@gmail.com>
2025-06-13 14:51:47 +0700

    fix(auth): remove unused import to fix build

--- src/components/shared/UserAuthButton.tsx
+++ src/components/shared/UserAuthButton.tsx
@@ -12,7 +12,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
